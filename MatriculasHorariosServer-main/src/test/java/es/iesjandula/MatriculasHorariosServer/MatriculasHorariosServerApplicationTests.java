55cd1a48c50752b1946f956c24175d31115bd78e
PabloDev09 <146177018+PabloDev09@users.noreply.github.com>
2024-12-07 16:13:49 +0100

    V3.1.0 - MatriculasHorariosServer - Eliminación de carpeta MatriculasHorariosServer-main

--- MatriculasHorariosServer-main/src/test/java/es/iesjandula/MatriculasHorariosServer/MatriculasHorariosServerApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package es.iesjandula.MatriculasHorariosServer;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class MatriculasHorariosServerApplicationTests 
-{
-
-}
